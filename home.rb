f261ac907f08b99e21c4709d33028cd0be95b655
Jason Hogrefe <hogrefejason@gmail.com>
2015-02-10 16:05:09 -0600

    Updated erb code.

--- home.rb
+++ home.rb
@@ -1,38 +1,18 @@
 require 'sinatra'
 
-# get "/greeting" do
-#   @name = "Jason"
-#
-#   #find the ERB file in views/welcome.erb and return it.
-#   erb :reginfo, :layout => :boilerplate
-# end
-#
-# get "/hello/:name" do
-#   params[:name]
-# end
-
-##################
-
-
 
 get "/" do
-  erb :welcome, :layout => :boilerplate 
+  erb :form, :layout => :boilerplate 
 end
 
 get "/info" do
   
   logger.info params
   
-  person = params["firstname", "lastname", "male", "female", "mystery", 
-    "fantasy", "crime", "nonfiction", "Books", "Movies", "Magazines"]
-  "#{firstname}"
+  "The new card holder is #{params["firstname", "lastname", "male", "female", "mystery", 
+    "fantasy", "crime", "nonfiction", "Books", "Movies", "Magazines"]}"
 end
 
-# get "/greet/:name" do
-#
-#   logger.info params
-#   person = params["my_name"]
-#   "Hello, #{person}"
-# end
+
 
 
